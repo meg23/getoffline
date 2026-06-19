d9b2db8897847d2384a7a552a1e87b8a77666d55
Max Gelman <814859+meg23@users.noreply.github.com>
2026-06-18 19:45:21 -0500

    Use models as shared Django app label

--- src/models/apps.py
+++ src/models/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class SharedModelsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "models"
-    label = "getoffline_models"
+    label = "models"
